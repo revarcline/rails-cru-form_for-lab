286f7ea44d1920db5483fb3af19478d2ddf3dc8e
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-17 00:42:05 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+rock = Genre.create(name: 'Rock')
+pop = Genre.create(name: 'Pop')
+rap = Genre.create(name: 'Rap')
+
+nirvana = Artist.create(name: 'Nirvana', bio: 'These guys made grunge big')
+britney = Artist.create(name: 'Britney Spears', bio: 'Her dad has held her captive for over a decade')
+ghostface = Artist.create(name: 'Ghostface Killah',
+                          bio: 'a.k.a Toney Starks a.k.a. Pretty Toney a.k.a. Ghostdini a.k.a. Starky Love')
+
+Song.create(name: 'Mighty Healthy', artist: ghostface, genre: rap)
+Song.create(name: 'Shakey Dog', artist: ghostface, genre: rap)
+Song.create(name: 'Crazy', artist: britney, genre: pop)
+Song.create(name: 'Toxic', artist: britney, genre: pop)
+Song.create(name: 'Come As You Are', artist: nirvana, genre: rock)
+Song.create(name: 'Heart Shaped Box', artist: nirvana, genre: rock)
